c5fd820e44d4d30dce1b2bc6756ef7cd35b08b4a
AllanZter <santosgrupo1000@gmail.com>
2025-04-21 21:32:54 -0300

    Feita herança de Clientes e classe Funcionarios

--- src/br/com/sistema/model/Funcionarios.java
+++ src/br/com/sistema/model/Funcionarios.java
@@ -1,25 +1,12 @@
 package br.com.sistema.model;
 
-public class Funcionarios {
-    private int id; 
-    private String nome;
-    private String apelido;   
-    private String rg; 
-    private String cpf; 
-    private String email;
+public class Funcionarios extends Clientes{
+    
+    private String apelido;       
     private String senha;
     private String cargo;
     private String nivel_acesso;
-    private String telefone; 
-    private String celular; 
-    private String cep; 
-    private String endereco; 
-    private int numero;
-    private String complemento;
-    private String bairro;
-    private String cidade;
-    private String estado;
- 
+
     public String getApelido() {
         return apelido;
     }
@@ -27,44 +14,13 @@ public class Funcionarios {
     public void setApelido(String apelido) {
         this.apelido = apelido;
     }
-    public int getId() {
-        return id;
+
+    public String getSenha() {
+        return senha;
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getRg() {
-        return rg;
-    }
-
-    public void setRg(String rg) {
-        this.rg = rg;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
+    public void setSenha(String senha) {
+        this.senha = senha;
     }
 
     public String getCargo() {
@@ -78,91 +34,7 @@ public class Funcionarios {
     public String getNivel_acesso() {
         return nivel_acesso;
     }
-
     public void setNivel_acesso(String nivel_acesso) {
         this.nivel_acesso = nivel_acesso;
-    }
-    public String getSenha(){
-        return senha;
-    }
-    public void setSenha(String senha) {
-        this.senha = senha;
-    }
-
-    public String getTelefone() {
-        return telefone;
-    }
-
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
-    }
-
-    public String getCelular() {
-        return celular;
-    }
-
-    public void setCelular(String celular) {
-        this.celular = celular;
-    }
-
-    public String getCep() {
-        return cep;
-    }
-
-    public void setCep(String cep) {
-        this.cep = cep;
-    }
-
-    public String getEndereco() {
-        return endereco;
-    }
-
-    public void setEndereco(String endereco) {
-        this.endereco = endereco;
-    }
-
-    public int getNumero() {
-        return numero;
-    }
-
-    public void setNumero(int numero) {
-        this.numero = numero;
-    }
-
-    public String getComplemento() {
-        return complemento;
-    }
-
-    public void setComplemento(String complemento) {
-        this.complemento = complemento;
-    }
-
-    public String getBairro() {
-        return bairro;
-    }
-
-    public void setBairro(String bairro) {
-        this.bairro = bairro;
-    }
-
-    public String getCidade() {
-        return cidade;
-    }
-
-    public void setCidade(String cidade) {
-        this.cidade = cidade;
-    }
-
-    public String getEstado() {
-        return estado;
-    }
-
-    public void setEstado(String estado) {
-        this.estado = estado;
-    }
-    
-    
-    
-    
-    
+    }                     
 }
